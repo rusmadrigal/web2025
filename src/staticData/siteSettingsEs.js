dccee435dcbcb2e6351088d341f32a43dcce8f6f
Rus <rusbenmadrigal@gmail.com>
2025-03-30 12:47:20 -0600

    Refactor siteSettingsEs.js to use relative URLs and selectors for Spanish site navigation

--- src/staticData/siteSettingsEs.js
+++ src/staticData/siteSettingsEs.js
@@ -17,72 +17,67 @@ import { BiCommentDetail } from "react-icons/bi";
 import { FaRegUser, FaYoutube } from "react-icons/fa6";
 import { MdFormatListBulleted } from "react-icons/md";
 
-const baseUrl =
-  typeof window !== "undefined"
-    ? window.location.origin
-    : "https://rusmadrigal.com";
-
 export const siteSettings = {
   logo: {
     image: logo?.src,
     alt: "Rus Madrigal",
-    url: "/es-cr",
+    url: "/es-cr", // Ruta relativa
     text: "Rus Madrigal",
   },
   headersMenu: [
     {
       id: 1,
       title: "Inicio",
-      selector: `${baseUrl}/es-cr/#inicio`,
-      url: `${baseUrl}/es-cr`,
+      selector: "/es-cr/#inicio", // Ruta relativa
+      url: "/es-cr", // Ruta relativa
       Icon: <IoHomeOutline />,
       notVisibleRoutes: [],
     },
     {
       id: 2,
       title: "Acerca",
-      selector: `${baseUrl}/es-cr/#acerca`,
-      url: `${baseUrl}/es-cr`,
+      selector: "/es-cr/#acerca", // Ruta relativa
+      url: "/es-cr", // Ruta relativa
       Icon: <FaRegUser />,
       notVisibleRoutes: [],
     },
     {
       id: 3,
       title: "Servicios",
-      selector: `${baseUrl}/es-cr/#servicios`, // Actualizado
-      url: `${baseUrl}/es-cr`,
+      selector: "/es-cr/#servicios", // Ruta relativa
+      url: "/es-cr", // Ruta relativa
       Icon: <IoBriefcaseOutline />,
       notVisibleRoutes: [],
     },
     {
       id: 4,
-      title: "https://rusmadrigal.com/es-cr/Skills",
-      selector: `${baseUrl}/es-cr/#skill`, // Actualizado
-      url: `${baseUrl}/es-cr`,
+      title: "Skills",
+      selector: "/es-cr/#skill", // Ruta relativa
+      url: "/es-cr", // Ruta relativa
       Icon: <SlGraduation />,
       notVisibleRoutes: [],
     },
     {
       id: 5,
       title: "CV",
-      selector: `${baseUrl}/es-cr/#resume`, // Actualizado
-      url: `${baseUrl}/es-cr`,
+      selector: "/es-cr/#resume", // Ruta relativa
+      url: "/es-cr", // Ruta relativa
       Icon: <FaRegFileAlt />,
       notVisibleRoutes: [],
     },
     {
       id: 8,
       title: "Opiniones",
-      selector: `${baseUrl}/es-cr/#opiniones`, // Actualizado
-      url: `${baseUrl}/es-cr`,
+      selector: "/es-cr/#opiniones", // Ruta relativa
+      url: "/es-cr", // Ruta relativa
       Icon: <BiCommentDetail />,
       notVisibleRoutes: [],
     },
     {
       id: 9,
       title: "Contacto",
-      selector: `${baseUrl}/es-cr/#contacto`, // Actualizado
-      url: `${baseUrl}/es-cr`,
+      selector: "/es-cr/#contacto", // Ruta relativa
+      url: "/es-cr", // Ruta relativa
       Icon: <FaRegEnvelope />,
       notVisibleRoutes: [],
     },
