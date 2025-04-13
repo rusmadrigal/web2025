73d95d24b602444cdec28769925c4fd184f27145
Rus <rusbenmadrigal@gmail.com>
2025-04-13 16:04:56 -0600

    Update

--- src/app/es-cr/layout.jsx
+++ src/app/es-cr/layout.jsx
@@ -6,17 +6,15 @@ import "swiper/css";
 
 import { Poppins } from "next/font/google";
 import dynamic from "next/dynamic";
-import Script from "next/script";
 
 const CustomCursor = dynamic(
   () => import("@/src/components/shared/CustomCursor"),
   { ssr: false }
 );
 
-const BackToTop = dynamic(
-  () => import("@/src/components/shared/BackToTop"),
-  { ssr: false }
-);
+const BackToTop = dynamic(() => import("@/src/components/shared/BackToTop"), {
+  ssr: false,
+});
 
 const poppins = Poppins({
   weight: ["200", "300", "400", "500", "600", "700"],
@@ -36,40 +34,10 @@ export const metadata = {
 
 export default function SpanishLayout({ children }) {
   return (
-    <html lang="es-CR" className="dark scroll-smooth" suppressHydrationWarning>
-      <head>
-        {/* GTM script */}
-        <Script
-          id="gtm-head"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-K2HNLR7W');
-            `,
-          }}
-        />
-      </head>
-      <body className={`${poppins.className} relative`}>
-        {/* GTM noscript fallback */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K2HNLR7W"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
-        <main>
-          {children}
-          <CustomCursor />
-          <BackToTop />
-        </main>
-      </body>
-    </html>
+    <>
+      {children}
+      <CustomCursor />
+      <BackToTop />
+    </>
   );
 }
